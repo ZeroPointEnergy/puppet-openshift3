9681995bb34cbb025430388d2fc053c1183e5690
root <root@eszpl10310.echbruedom.local>
2015-09-23 15:06:57 +0000

    Changes made during accarda PoC.

--- manifests/ansible.pp
+++ manifests/ansible.pp
@@ -4,7 +4,7 @@ class openshift3::ansible {
     ensure   => latest,
     provider => git,
     source   => "https://github.com/openshift/openshift-ansible.git",
-    revision => 'master',
+    revision => 'openshift-ansible-3.0.19-1',
   } ->
 
   file { "/etc/ansible":
